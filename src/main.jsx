983cc5b7209221c12fd092d498acc3eff78e5f59
Raisul Kayes Raka <119594099+RaisulKayesRaka@users.noreply.github.com>
2025-01-02 20:51:41 +0600

    Add ErrorPage

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,21 @@ import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
+import ErrorPage from "./pages/ErrorPage.jsx";
+import { HelmetProvider } from "react-helmet-async";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    errorElement: <ErrorPage />,
   },
 ]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>
+    <HelmetProvider>
+      <RouterProvider router={router} />
+    </HelmetProvider>
+  </StrictMode>,
 );
