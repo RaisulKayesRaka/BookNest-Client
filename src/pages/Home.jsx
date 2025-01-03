54d9b9d608d2f278819d4702b886f13ec33e305c
Raisul Kayes Raka <119594099+RaisulKayesRaka@users.noreply.github.com>
2025-01-03 22:33:02 +0600

    Add FAQ

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { Helmet } from "react-helmet-async";
 import Banner from "../components/Banner";
 import LibraryInsights from "../components/LibraryInsights";
+import FAQ from "../components/FAQ";
 
 export default function Home() {
   return (
@@ -10,6 +11,8 @@ export default function Home() {
       </Helmet>
       <Banner />
       <LibraryInsights />
+      <hr className="mx-auto w-1/2 max-w-prose border-blue-500" />
+      <FAQ />
     </>
   );
 }
