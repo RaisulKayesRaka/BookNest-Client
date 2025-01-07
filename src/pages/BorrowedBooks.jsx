b9d04f83f0f79da3f65f69093ea721b694e1390f
Raisul Kayes Raka <119594099+RaisulKayesRaka@users.noreply.github.com>
2025-01-07 09:54:05 +0600

    Add message in BorrowedBooks page when no books found

--- src/pages/BorrowedBooks.jsx
+++ src/pages/BorrowedBooks.jsx
@@ -35,8 +35,7 @@ export default function BorrowedBooks() {
       );
       if (data.deletedCount === 1) {
         toast.success("Book returned successfully");
-        const updatedBooks = books.filter((book) => book._id !== bookId);
-        setBooks(updatedBooks);
+        setBooks(books.filter((book) => book._id !== bookId));
       }
     } catch (error) {
       console.log(error);
@@ -55,46 +54,54 @@ export default function BorrowedBooks() {
         {loading ? (
           <Loading />
         ) : (
-          <section className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-4">
-            {books.map((book) => (
-              <div
-                key={book?._id}
-                className="flex flex-col gap-4 rounded-xl border p-4 shadow"
-              >
-                <img
-                  src={book?.image}
-                  className="aspect-video w-full rounded-md object-contain"
-                  alt="Book Cover"
-                />
-                <p className="rounded-lg bg-blue-100 px-4 py-1 text-center text-sm font-semibold text-blue-600">
-                  {book?.category}
-                </p>
-                <div className="flex-1">
-                  <h3 className="text-center font-bold transition-colors">
-                    {book?.name}
-                  </h3>
-                </div>
+          <>
+            {books.length > 0 ? (
+              <section className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-4">
+                {books.map((book) => (
+                  <div
+                    key={book?._id}
+                    className="flex flex-col gap-4 rounded-xl border p-4 shadow"
+                  >
+                    <img
+                      src={book?.image}
+                      className="aspect-video w-full rounded-md object-contain"
+                      alt="Book Cover"
+                    />
+                    <p className="rounded-lg bg-blue-100 px-4 py-1 text-center text-sm font-semibold text-blue-600">
+                      {book?.category}
+                    </p>
+                    <div className="flex-1">
+                      <h3 className="text-center font-bold transition-colors">
+                        {book?.name}
+                      </h3>
+                    </div>
 
-                <div className="space-y-1 rounded-lg bg-blue-50 p-4">
-                  <p className="">
-                    <span className="font-semibold">Borrowed Date:</span>{" "}
-                    {book?.borrowDate}
-                  </p>
-                  <p className="">
-                    <span className="font-semibold">Return Date:</span>{" "}
-                    {book?.returnDate}
-                  </p>
-                </div>
+                    <div className="space-y-1 rounded-lg bg-blue-50 p-4">
+                      <p className="">
+                        <span className="font-semibold">Borrowed Date:</span>{" "}
+                        {book?.borrowDate}
+                      </p>
+                      <p className="">
+                        <span className="font-semibold">Return Date:</span>{" "}
+                        {book?.returnDate}
+                      </p>
+                    </div>
 
-                <button
-                  onClick={() => handleReturnBook(book?._id)}
-                  className="w-full rounded-lg bg-blue-500 px-4 py-1.5 font-semibold text-white transition-colors hover:bg-blue-600"
-                >
-                  Return
-                </button>
-              </div>
-            ))}
-          </section>
+                    <button
+                      onClick={() => handleReturnBook(book?._id)}
+                      className="w-full rounded-lg bg-blue-500 px-4 py-1.5 font-semibold text-white transition-colors hover:bg-blue-600"
+                    >
+                      Return
+                    </button>
+                  </div>
+                ))}
+              </section>
+            ) : (
+              <h2 className="text-center text-gray-400">
+                You currently have no books borrowed.
+              </h2>
+            )}
+          </>
         )}
       </section>
     </>
